584913a2bacd2cc984aff9ccdec27f76e0dd8be8
itsmatsouza <mateus.h1055@gmail.com>
2023-05-09 22:04:44 -0300

    configurando adminjs e adicionando a db categorias

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express from "express";
-import { sequelize } from "../database";
+import { sequelize } from "./database";
+import { adminJs, adminJsRouter } from "./adminjs";
 
 const app = express();
+
+app.use(adminJs.options.rootPath, adminJsRouter);
+
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
   await sequelize.authenticate().then(() => {
-    console.log('DB Connection Successfully')
-  })
+    console.log("DB Connection Successfully");
+  });
   console.log(`Server started successfully at port ${PORT}`);
 });
