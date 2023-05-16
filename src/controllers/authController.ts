f46cba9809d6e17f7ee8f4f1bfcd7c2a8ee7a0c7
itsmatsouza <mateus.h1055@gmail.com>
2023-05-16 16:23:58 -0300

    adicionando login com json web token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { userService } from "../services/userService";
+import { jwtService } from "../services/jwtService";
 
 export const authController = {
 
@@ -25,6 +26,38 @@ export const authController = {
       })
 
       return res.status(201).json(user)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+
+  // POST /auth/login
+  login: async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    try {
+      const user = await userService.findByEmail(email)
+
+      if (!user) return res.status(404).json({ message: 'E-mail não registrado!'})
+
+      user.checkPassword(password, (err, isSame) => {
+        if (err) return res.status(400).json({ message: err.message})
+
+        if (!isSame) return res.status(401).json({ message: 'Senha incorreta!'})
+
+        // payload sao as propriedades que vao ser geradas dentro do token, OBS: nao colocar informacoes importantes dos usuarios
+        const payload = {
+          id: user.id,
+          firstName: user.firstName,
+          email: user.email
+        }
+
+        const token = jwtService.signToken(payload, '7d') // 7 dias
+
+        return res.json({ authenticated: true, ...payload, token })
+      })
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message });
