c6f3da39d6774677d2006a435b4a33715649413a
itsmatsouza <mateus.h1055@gmail.com>
2023-05-17 22:49:53 -0300

    adicionando favoritos ( minha lista )

---
+++ src/models/Favorite.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model } from "sequelize"
+import { CourseInstance } from "./Courses"
+import { UserInstance } from "./User"
+import { sequelize } from "../database"
+
+export interface Favorite {
+  userId: number
+  courseId: number
+}
+
+export interface favoriteInstance extends Model<Favorite>, Favorite {
+  course?: CourseInstance
+  user?: UserInstance
+}
+
+export const Favorite = sequelize.define<favoriteInstance, Favorite>('Favorite', {
+  userId: {
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'User', 
+      key: 'id'
+    },
+    onUpdate : 'CASCADE',
+    onDelete: 'CASCADE'
+  },
+  courseId: {
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'Courses', 
+      key: 'id'
+    },
+    onUpdate : 'CASCADE',
+    onDelete: 'CASCADE'
+  }
+})
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ import { Category } from "./Category"
 import { Course } from "./Courses"
 import { Episode } from "./Episode"
 import { User } from "./User"
+import { Favorite } from "./Favorite"
 
 Category.hasMany(Course, { as: 'courses' })
 Course.belongsTo(Category)
@@ -9,9 +10,17 @@ Course.belongsTo(Category)
 Course.hasMany(Episode, { as: 'episodes'}) // Episodes, forma padrao do sequelize
 Episode.belongsTo(Course)
 
+Course.belongsToMany(User, { through: Favorite})
+User.belongsToMany(Course, { through: Favorite})
+Course.hasMany(Favorite, { as: 'FavoritesUsers', foreignKey: 'course_id'})
+Favorite.belongsTo(Course)
+Favorite.belongsTo(User)
+User.hasMany(Favorite, { as: 'FavoritesCourses', foreignKey: 'user_id'})
+
 export {
   Category,
   Course,
   Episode,
+  Favorite,
   User
 }
