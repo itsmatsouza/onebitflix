286ce21d51f15621cca641343e1a84d6af1e1d7f
itsmatsouza <mateus.h1055@gmail.com>
2023-05-21 16:53:44 -0300

    adicionando se curtido e se favoritado nos cursos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -39,5 +39,16 @@ export const favoriteService = {
         courseId: courseId
       }
     })
+  },
+
+  isFavorited: async (userId: number, courseId:number) => {
+    const favorite = await Favorite.findOne({
+      where: {
+        userId: userId,
+        courseId: courseId
+      }
+    })
+
+    return favorite !== null ? true : false
   }
 }
