c483106a236a713abc67c40ebcb8957e8c391d46
itsmatsouza <mateus.h1055@gmail.com>
2023-05-21 12:51:52 -0300

    rota adicionando e removendo likes em cursos

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,13 +3,19 @@ import { Course } from "./Courses"
 import { Episode } from "./Episode"
 import { User } from "./User"
 import { Favorite } from "./Favorite"
+import { Like } from "./Like"
 
+// Categoria tem muitos cursos e um curso pertence a uma categoria
 Category.hasMany(Course, { as: 'courses' })
 Course.belongsTo(Category)
 
+// Curso tem muitos episodios e um episodio pertence a um curso
 Course.hasMany(Episode, { as: 'episodes'}) // Episodes, forma padrao do sequelize
 Episode.belongsTo(Course)
 
+// Cada usuario pode marcar muitos cursos como favorito, o adicionando na tabela favorite, linhas
+// Muitos cursos podem ser marcados como favorito por muitos usuarios, o adicionando na tabela favorite, linhas 
+// A tabela favorite tem como associacao a tabela course e a tabela user, linhas: 22, 23
 Course.belongsToMany(User, { through: Favorite})
 User.belongsToMany(Course, { through: Favorite})
 Course.hasMany(Favorite, { as: 'FavoritesUsers', foreignKey: 'course_id'})
@@ -17,10 +23,16 @@ Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 User.hasMany(Favorite, { as: 'FavoritesCourses', foreignKey: 'user_id'})
 
+// Cada usuario pode marcar muitos cursos como favorito, o adicionando na tabela like
+// Cada curso pode ter muitos usuarios que o marcaram como favorito, o adicionando na tabela like
+Course.belongsToMany(User, { through: Like })
+User.belongsToMany(Course, { through: Like })
+
 export {
   Category,
   Course,
   Episode,
   Favorite,
+  Like,
   User
 }
