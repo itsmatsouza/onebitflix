6a60e17941f77a9006f0f1da384c68c5af3bbc0d
itsmatsouza <mateus.h1055@gmail.com>
2023-05-21 17:13:39 -0300

    obtendo os 10 cursos mais popularres

--- src/routes.ts
+++ src/routes.ts
@@ -19,6 +19,7 @@ router.get('/categories/:id', ensureAuth, categoriesController.show)
 // O express testa as rotas em ordem, todas as rotas com variavel dinamicas (ex: :id), devem ficar abaixo das demais
 router.get('/courses/featured', ensureAuth, coursesController.featured)
 router.get('/courses/newest', coursesController.newest)
+router.get('/courses/popular',ensureAuth, coursesController.popular)
 router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
