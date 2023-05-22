dd3399ea91ad3612ba3383b648de61ddbd8b28ba
itsmatsouza <mateus.h1055@gmail.com>
2023-05-21 23:31:32 -0300

    atualizando senha do usuario

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -60,6 +60,16 @@ export const userService = {
     return updatedUsers[0]
   },
 
+  updatePassword: async (id: number, password: string) => {
+    const [affectedRows, updatedUsers] = await User.update({password}, {
+      where: {id: id},
+      returning: true,
+      individualHooks: true
+    })
+
+    return updatedUsers[0]
+  },
+
   getKeepWatchingList: async (id: number) => {
     const userWithWatchingEpisodes = await User.findByPk(id, {
       include: {
